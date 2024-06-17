d978da5de70dcd4b74ca25fdea3c7538ad267f4c
shivanisingh1804 <shivani1804singh@gmail.com>
2024-06-17 08:50:45 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,307 @@
+// // package main
+
+// // import (
+// // 	"context"
+// // 	"encoding/json"
+// // 	"fmt"
+// // 	"log"
+// // 	"os"
+
+// // 	"github.com/joho/godotenv"
+// // 	"github.com/shomali11/slacker"
+// // 	"github.com/tidwall/gjson"
+// // 	witai "github.com/wit-ai/wit-go/v2"
+// // )
+
+// // func printCommandEvents(analysisChannel <-chan *slacker.CommandEvent) {
+// // 	for event := range analysisChannel {
+// // 		fmt.Println("command events")
+// // 		fmt.Println(event.Timestamp)
+// // 		fmt.Println(event.Command)
+// // 		fmt.Println(event.Parameters)
+// // 		fmt.Println(event.Event)
+// // 		fmt.Println()
+
+// // 	}
+// // }
+// // func main() {
+// // 	godotenv.Load(".env")
+// // 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP-TOKEN"))
+// // 	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+// // 	go printCommandEvents(bot.CommandEvents())
+
+// // 	bot.Command("query for bot-<meesage>", &slacker.CommandDefinition{
+// // 		Description: "send any quest to wolframe",
+// // 		Examples:    "who is the president of india",
+// // 		Handler: func(bc slacker.BotContext, r slacker.Request, w slacker.ResponseWriter) {
+// // 			query := request.Param("message")
+// // 			fmt.Println(query)
+// // 			msg, _ := client.Parse(&witai.MessageRequest{
+// // 				Query: query,
+// // 			})
+// // 			data, _ := json.MarshalIndent(msg, "", "    ")
+// // 			rough := string(data[:])
+// // 			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query")
+// // 			fmt.Println(rough)
+// // 			response.Reply("recieved")
+// // 		},
+// // 	})
+
+// //		ctx, cancel := context.WithCancel(context.Background())
+// //		defer cancel()
+// //		err := bot.Listen(ctx)
+// //		if err != nil {
+// //			log.Fatal(err)
+// //		}
+// //	}
+// package main
+
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// 	"github.com/shomali11/slacker"
+// 	"github.com/tidwall/gjson"
+// 	witai "github.com/wit-ai/wit-go/v2"
+// )
+
+// func printCommandEvents(analysisChannel <-chan *slacker.CommandEvent) {
+// 	for event := range analysisChannel {
+// 		fmt.Println("command events")
+// 		fmt.Println(event.Timestamp)
+// 		fmt.Println(event.Command)
+// 		fmt.Println(event.Parameters)
+// 		fmt.Println(event.Event)
+// 		fmt.Println()
+// 	}
+// }
+
+// func main() {
+// 	godotenv.Load(".env")
+// 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+// 	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+// 	go printCommandEvents(bot.CommandEvents())
+
+// 	bot.Command("query for bot-<message>", &slacker.CommandDefinition{
+// 		Description: "send any quest to wolframe",
+// 		Examples:    "who is the president of india",
+// 		Handler: func(bc slacker.BotContext, r slacker.Request, w slacker.ResponseWriter) {
+// 			query := r.Param("message")
+// 			fmt.Println(query)
+// 			msg, _ := client.Parse(&witai.MessageRequest{
+// 				Query: query,
+// 			})
+// 			data, _ := json.MarshalIndent(msg, "", "    ")
+// 			rough := string(data[:])
+// 			value := gjson.Get(rough, "entities.wit$wolfram_search_query.0.value")
+// 			fmt.Println(rough)
+// 			fmt.Println(value.String())
+// 			w.Reply("received")
+// 		},
+// 	})
+
+//		ctx, cancel := context.WithCancel(context.Background())
+//		defer cancel()
+//		err := bot.Listen(ctx)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	}
+// package main
+
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// 	"github.com/shomali11/slacker"
+// 	"github.com/tidwall/gjson"
+// 	witai "github.com/wit-ai/wit-go/v2"
+// )
+
+// func printCommandEvents(analysisChannel <-chan *slacker.CommandEvent) {
+// 	for event := range analysisChannel {
+// 		fmt.Println("command events")
+// 		fmt.Println(event.Timestamp)
+// 		fmt.Println(event.Command)
+// 		fmt.Println(event.Parameters)
+// 		fmt.Println(event.Event)
+// 		fmt.Println()
+// 	}
+// }
+
+// func main() {
+// 	godotenv.Load(".env")
+// 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+// 	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+// 	go printCommandEvents(bot.CommandEvents())
+
+// 	bot.Command("query for bot-<message>", &slacker.CommandDefinition{
+// 		Description: "send any quest to wolframe",
+// 		Examples:    []string{"who is the president of india"},
+// 		Handler: func(bc slacker.BotContext, r slacker.Request, w slacker.ResponseWriter) {
+// 			query := r.Param("message")
+// 			fmt.Println(query)
+// 			msg, _ := client.Parse(&witai.MessageRequest{
+// 				Query: query,
+// 			})
+// 			data, _ := json.MarshalIndent(msg, "", "    ")
+// 			rough := string(data[:])
+// 			value := gjson.Get(rough, "entities.wit$wolfram_search_query.0.value")
+// 			fmt.Println(rough)
+// 			fmt.Println(value.String())
+// 			w.Reply("received")
+// 		},
+// 	})
+
+//		ctx, cancel := context.WithCancel(context.Background())
+//		defer cancel()
+//		err := bot.Listen(ctx)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	}
+// package main
+
+// import (
+// 	"context"
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// 	"github.com/krognol/go-wolfram"
+// 	"github.com/shomali11/slacker"
+// 	"github.com/tidwall/gjson"
+
+// 	witai "github.com/wit-ai/wit-go/v2"
+// )
+
+// var wolframClient *wolfram.Client
+
+// func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+// 	for event := range analyticsChannel {
+// 		fmt.Println("Command Events")
+// 		fmt.Println(event.Timestamp)
+// 		fmt.Println(event.Command)
+// 		fmt.Println(event.Parameters)
+// 		fmt.Println(event.Event)
+// 		fmt.Println()
+// 	}
+// }
+
+// func main() {
+// 	godotenv.Load(".env")
+
+// 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+// 	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+// 	wolframClient := &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
+// 	go printCommandEvents(bot.CommandEvents())
+
+// 	bot.Command("query for bot - <message>", &slacker.CommandDefinition{
+// 		Description: "send any question to wolfram",
+// 		Examples:    []string{"who is the president of india"},
+// 		// Example:     "who is the president of india",
+// 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+// 			query := request.Param("message")
+
+// 			msg, _ := client.Parse(&witai.MessageRequest{
+// 				Query: query,
+// 			})
+// 			data, _ := json.MarshalIndent(msg, "", "    ")
+// 			rough := string(data[:])
+// 			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
+// 			answer := value.String()
+// 			res, err := wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
+// 			if err != nil {
+// 				fmt.Println("there is an error")
+// 			}
+// 			fmt.Println(value)
+// 			response.Reply(res)
+// 		},
+// 	})
+
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+
+// 	err := bot.Listen(ctx)
+
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	}
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/krognol/go-wolfram"
+	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
+	witai "github.com/wit-ai/wit-go/v2"
+)
+
+var wolframClient *wolfram.Client
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {
+	godotenv.Load(".env")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	client := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+	wolframClient = &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")} // use the package-level variable
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("query for bot - <message>", &slacker.CommandDefinition{
+		Description: "send any question to wolfram",
+		Examples:    []string{"who is the president of india"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			query := request.Param("message")
+
+			msg, _ := client.Parse(&witai.MessageRequest{
+				Query: query,
+			})
+			data, _ := json.MarshalIndent(msg, "", "    ")
+			rough := string(data[:])
+			value := gjson.Get(rough, "entities.wit$wolfram_search_query:wolfram_search_query.0.value")
+			answer := value.String()
+			res, err := wolframClient.GetSpokentAnswerQuery(answer, wolfram.Metric, 1000)
+			if err != nil {
+				fmt.Println("there is an error")
+			}
+			fmt.Println(value)
+			response.Reply(res)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
